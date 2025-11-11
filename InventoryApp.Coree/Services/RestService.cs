e66b984053b421dca1b56ca81474345afc715a77
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2025-11-11 13:02:02 +0100

    alles

--- InventoryApp.Coree/Services/RestService.cs
+++ InventoryApp.Coree/Services/RestService.cs
@@ -1,6 +1,8 @@
 ﻿using InventoryApp.Coree.Models;
+using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,34 @@ namespace InventoryApp.Coree.Services
 {
     public class RestService : IRepository
     {
+        private RestClient _client;
+        public RestService()
+        {
+            var options = new RestClientOptions("http://inventory.test/api/")
+            {
+                ThrowOnAnyError = true,
+                Timeout = new TimeSpan(0,2,0)
+            };
+            this._client = new RestClient(options);
+            this._client.AddDefaultHeaders(new Dictionary<string, string>()
+            {
+                {KnownHeaders.ContentType, "application/json" },
+                {KnownHeaders.Accept, "application/json" },
+
+            });
+        }
+
         public List<InventoryItem> Load()
         {
-            throw new NotImplementedException();
+            var request = new RestRequest("/items", Method.Get);
+            var result = this._client.Get<InventoryResponse>(request);
+
+            System.Diagnostics.Debug.WriteLine(result);
+            return new List<InventoryItem>()
+            {
+                new InventoryItem("INV123", "Test"),
+                new InventoryItem("INV343", "Probe")
+            };
         }
     }
 }
